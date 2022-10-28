e0cb169bd55cb5c5dcfe2f2ce7aa392be2d12466
Emmanuel Hateka <emmanuelhateka.com>
2022-10-28 23:05:48 +0000

    socket.io real time chat in progress

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -8,7 +8,51 @@ export const socket = (server) => {
     },
   });
 
+  let users = [];
+
+  //  user id will be sent from the client
+  //   SocketId is the socket id of the user connected
+  const addOnlineUsers = (userId, socketId) => {
+    users.forEach((user) => {
+      if (user.userId !== userId) {
+        users = users.push(userId, socketId);
+        console.log(users);
+        return users;
+      }
+    });
+  };
+
+  const removeOfflineUsers = (socketId) => {
+    users = users.filter((user) => {
+      return user.socketId !== socketId;
+    });
+  };
+
+  const findUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+  };
+
   io.on("connection", (socket) => {
-    console.log(socket.id);
+    socket.on("addUser", (userId) => {
+      // console.log(socket.id);
+      addOnlineUsers(userId, socket.id);
+      socket.emit("getUsers", addOnlineUsers);
+    });
+    socket.on("disconnect", (socketId) => {
+      removeOfflineUsers(socketId);
+      socket.emit("getUsers", addOnlineUsers);
+    });
+    socket.on(
+      "sendMessage",
+      ({ userId, recieverId, text, videos, pictures }) => {
+        const user = findUser(recieverId);
+        socket.to(user.socketId).emit("getMessage", {
+          userId,
+          text,
+          videos,
+          pictures,
+        });
+      }
+    );
   });
 };
